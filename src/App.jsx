152fd80d7cd9aebfbb6237b53b75d95425bd8023
ANASS BENDARSI <bendarsi@gmail.com>
2021-05-15 16:45:44 +0000

    delete image from images list

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,48 @@
 import React, { useState } from 'react';
+import _ from 'lodash';
+
+import './App.css';
 
 const App = () => {
-  const [images, setImages] = useState([]);
+  const [images, setImages] = useState({});
 
   const onSubmit = e => {
     e.preventDefault();
-    console.log(e.target.images.files);
-    console.log(typeof e.target.images.files);
-    setImages(e.target.images.files);
   };
+
+  const onChange = e => {
+    console.log('change', e.target.files);
+    const newFiles = _.mapKeys(e.target.files, value => value.name);
+    setImages({ ...images, ...newFiles });
+    console.log({ ...images, ...newFiles });
+  };
+
+  const onRemove = (name) => {
+    console.log('remove', name)
+    const nextImages = {...images};
+    delete nextImages[name]
+    setImages(nextImages);
+  }
 
   return (
     <div>
-      {images.length}
       <form onSubmit={onSubmit} encType='multipart/form-data'>
-        <input name='images' type='file' multiple />
+        <input name='images' type='file' multiple onChange={onChange} />
         <input type='submit' value='submit' />
       </form>
       <div>
-        {Object.values(images).map(image => (
-          <img key={image.name} src={URL.createObjectURL(image)} alt='pic' />
-        ))}
+        <div className='images-container'>
+          {Object.values(images).map(image => (
+            <div className='image-card'>
+              <img
+                key={image.name}
+                src={URL.createObjectURL(image)}
+                alt='pic'
+              />
+              <i className="gg-close" onClick={() => onRemove(image.name)}></i>
+            </div>
+          ))}
+        </div>
       </div>
     </div>
   );
