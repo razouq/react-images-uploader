186a91609dca5c3f852afa6813e4d76a52f9689f
ANASS BENDARSI <bendarsi@gmail.com>
2021-05-19 18:45:00 +0100

    design the form, get the submitted ids

--- src/App.jsx
+++ src/App.jsx
@@ -1,82 +1,73 @@
 import React, { useState } from 'react';
 import _ from 'lodash';
+import { v4 as uuidv4 } from 'uuid';
 import axios from 'axios';
-import { Image, Transformation } from 'cloudinary-react';
 
 import './App.css';
 
 const App = () => {
   const [images, setImages] = useState({});
-  const [imgs, setImgs] = useState([]);
 
   const onSubmit = e => {
     e.preventDefault();
+    console.log('submitted images Id', Object.keys(images));
   };
 
   const onChange = async e => {
-    console.log('change', e.target.files);
-    const newFiles = _.mapKeys(e.target.files, value => value.name);
-    setImages({ ...images, ...newFiles });
-    console.log({ ...images, ...newFiles });
+    const newImages = _.mapKeys(e.target.files, () => uuidv4());
+    setImages(prevImages => ({ ...prevImages, ...newImages }));
 
     const formData = new FormData();
     console.log('formdata');
-    const files = e.target.files;
-    for (let i = 0; i < files.length; i++) {
-      console.log(i);
-      let file = files[i];
-      formData.append('file', file);
+    for (const [imageKey, Image] of Object.entries(newImages)) {
+      formData.append('file', Image);
       formData.append('upload_preset', 'zjnbccsn');
+      formData.append('public_id', imageKey);
       try {
         const response = await axios.post(
           `https://api.cloudinary.com/v1_1/db1waj76f/image/upload`,
           formData,
         );
-        setImgs([...imgs, response.data])
-        console.log('response', response);
+        console.log(response);
       } catch (err) {
         console.log(err.response);
       }
     }
   };
 
-  const onRemove = name => {
-    console.log('remove', name);
+  const onRemove = imageKey => {
     const nextImages = { ...images };
-    delete nextImages[name];
+    delete nextImages[imageKey];
     setImages(nextImages);
   };
 
   return (
-    <div>
-      <form onSubmit={onSubmit} encType='multipart/form-data'>
-        <input name='images' type='file' multiple onChange={onChange} />
-        <input type='submit' value='submit' />
-      </form>
-      <div>
-        <div className='images-container'>
-          {Object.values(images).map(image => (
-            <div className='image-card'>
-              <img
-                key={image.name}
-                src={URL.createObjectURL(image)}
-                alt='pic'
-              />
-              <i className='gg-close' onClick={() => onRemove(image.name)}></i>
+    <div className='wrapper'>
+      <div className='container'>
+        <h1>Submit Images Form</h1>
+        <form onSubmit={onSubmit} encType='multipart/form-data'>
+          <input name='images' id='images' type='file' multiple onChange={onChange} />
+          <label htmlFor="images">Choose Images</label>
+          <div>
+            <div className='images-container'>
+              {Object.keys(images).map(ImageKey => (
+                <div className='image-card'>
+                  <img
+                    key={ImageKey}
+                    src={URL.createObjectURL(images[ImageKey])}
+                    alt='pic'
+                  />
+                  <i
+                    className='gg-close'
+                    onClick={() => onRemove(ImageKey)}
+                  ></i>
+                </div>
+              ))}
             </div>
-          ))}
-
-          {/* <Image publicId='sample'>
-            <Transformation width='200' crop='scale' angle='10' />
-          </Image> */}
-        </div>
-        <div className='images-container'>
-          {imgs.map(img => (
-            <Image cloudName="db1waj76f" publicId={img.public_id} key={img.public_id}>
-              <Transformation width='200' crop='scale' />
-            </Image>
-          ))}
-        </div>
+          </div>
+          <input type='submit' value='submit' />
+          <p>submitted images ids : <br/> {'[' + Object.keys(images).join(', ') + ']'}</p>
+        </form>
       </div>
     </div>
   );
